46c39244ae7c427f2bbd75ce0c3adf0aca0a20d5
Marc Nijdam <marc.nijdam@gmail.com>
2016-10-10 12:43:41 -0700

    Add dataset.append and associated unique and delete_by

--- geckoboard/dataset.py
+++ geckoboard/dataset.py
@@ -1,13 +1,14 @@
 from __future__ import unicode_literals
-from future.utils import iteritems
+from future.utils import viewitems, viewvalues
 from iso4217 import Currency
 
 
 class Field(object):
-    def __init__(self, type, name, currency_code=None):
+    def __init__(self, type, name, currency_code=None, unique=False):
         self.type = type
         self.name = name
         self.currency_code = currency_code
+        self.unique = unique
         if currency_code is not None:
             self._money = getattr(Currency, currency_code.lower())
 
@@ -15,6 +16,7 @@ class Field(object):
         return all([
             self.type == getattr(other, 'type', None),
             self.name == getattr(other, 'name', None),
+            self.unique == getattr(other, 'unique', None),
             self.currency_code == getattr(other, "currency_code", None)
         ])
 
@@ -22,35 +24,44 @@ class Field(object):
         return not self == other
 
     @staticmethod
-    def date(name):
-        return Field('date', name)
+    def date(name, unique=False):
+        return Field('date', name,
+                     unique=unique)
 
     @staticmethod
-    def datetime(name):
-        return Field('datetime', name)
+    def datetime(name, unique=False):
+        return Field('datetime', name,
+                     unique=unique)
 
     @staticmethod
-    def number(name):
-        return Field('number', name)
+    def number(name, unique=False):
+        return Field('number', name,
+                     unique=unique)
 
     @staticmethod
-    def percentage(name):
-        return Field('percentage', name)
+    def percentage(name, unique=False):
+        return Field('percentage', name,
+                     unique=unique)
 
     @staticmethod
-    def string(name):
-        return Field('string', name)
+    def string(name, unique=False):
+        return Field('string', name,
+                     unique=unique)
 
     @staticmethod
-    def money(name, currency_code):
-        return Field('money', name, currency_code=currency_code)
+    def money(name, currency_code, unique=False):
+        return Field('money', name,
+                     currency_code=currency_code,
+                     unique=unique)
 
     @classmethod
-    def from_schema(cls, json):
+    def from_schema(cls, json, unique=False):
         type = json.get('type')
         name = json.get('name')
         currency_code = json.get('currency_code', None)
-        return cls(type, name, currency_code=currency_code)
+        return cls(type, name,
+                   currency_code=currency_code,
+                   unique=unique)
 
     def to_schema(self):
         value = {
@@ -84,31 +95,58 @@ class Dataset(object):
     def from_json(cls, session, json):
         id = json.get('id')
         fields = json.get('fields')
-        fields = {f[0]: Field.from_schema(f[1]) for f in iteritems(fields)}
+        unique_fields = frozenset(json.get('unique_by', []))
+
+        def _build_field(name, value):
+            unique = name in unique_fields
+            return Field.from_schema(value, unique=unique)
+
+        fields = {f[0]: _build_field(f[0], f[1]) for f in viewitems(fields)}
         return cls(session, id, fields)
 
     @classmethod
     def create(cls, session, id, fields):
         json = {
-            'fields': {f[0]: f[1].to_schema() for f in iteritems(fields)}
+            'fields': {f[0]: f[1].to_schema() for f in viewitems(fields)}
         }
+        unique_by = [f[0] for f in viewitems(fields) if f[1].unique]
+        if unique_by:
+            json['unique_by'] = unique_by
         url = session.build_url(id)
+        print json
         response = session.put(url, json=json)
+        print response
         response.raise_for_status()
         return cls.from_json(session, response.json())
 
-    def replace(self, data):
+    def _build_data_json(self, data):
         def _fields_json(entry):
             fields = self.fields
             return {f[0]: fields[f[0]].to_json(f[1])
-                    for f in iteritems(entry)}
+                    for f in viewitems(entry)}
 
         json = {
             'data': [_fields_json(entry) for entry in data]
         }
+        return json
+
+    def replace(self, data):
         session = self._session
         url = session.build_url(self.id, 'data')
+        json = self._build_data_json(data)
+
         response = session.put(url, json=json)
+        response.raise_for_status()
+        return True
+
+    def append(self, data, delete_by=None):
+        session = self._session
+        url = session.build_url(self.id, 'data')
+        json = self._build_data_json(data)
+        if delete_by is not None:
+            json['delete_by'] = delete_by
+
+        response = session.post(url, json=json)
         response.raise_for_status()
         return True
 
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,7 +6,7 @@ from datetime import date, datetime, timedelta
 
 def test_create_delete(session):
     fields = {
-        'date': F.date('Date'),
+        'date': F.date('Date', unique=True),
         'datetime': F.datetime('Date Time'),
         'number': F.number('Number'),
         'percentage': F.percentage('Percentage'),
@@ -25,19 +25,27 @@ def test_create_delete(session):
     assert result.delete() is True
 
 
-def test_replace(tmp_dataset):
-    start_date = date(2016, 9, 23)
-
+def _tmp_data(start_date, count):
     def _day_date(day_offset):
         return start_date + timedelta(days=day_offset)
 
-    data = [{
+    return [{
         'date': _day_date(offset),
         'datetime': datetime(2016, 9, 23),
         'number': 22,
         'percentage': 0.5,
         'string': "test string",
         'money': 7.95
-    } for offset in range(5)]
+    } for offset in range(count)]
+
+
+def test_replace(tmp_dataset):
+    data = _tmp_data(date(2016, 9, 23), 5)
 
     assert tmp_dataset.replace(data) is True
+
+
+def test_append(tmp_dataset):
+    data = _tmp_data(date(2016, 9, 23), 5)
+
+    assert tmp_dataset.append(data, delete_by='date') is True
